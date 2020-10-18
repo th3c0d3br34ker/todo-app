56c59c2cdf016ee66d251c8d3db69f004b89e2d7
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2020-10-18 14:06:27 +0530

    Updates

---
+++ precache-manifest.3691e219416efdbf6ab70efa793c24d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f5f1e81058bbd3af0a8e7539832a9b",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "01ba48466a72c48f9099",
+    "url": "/todo-app/static/css/main.7c97f2ab.chunk.css"
+  },
+  {
+    "revision": "b9724e4ca64478a512d1",
+    "url": "/todo-app/static/js/2.66bd7bd8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/todo-app/static/js/2.66bd7bd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ba48466a72c48f9099",
+    "url": "/todo-app/static/js/main.d82a672f.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
